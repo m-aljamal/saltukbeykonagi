7c38b86e644b19ec08f4cedff3a1bd3bea31a809
m-aljamal <mohammadjamol@gmail.com>
2021-10-11 17:41:27 +0300

    finish

--- pages/iletisim.tsx
+++ pages/iletisim.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "src/components/Form";
 import HeroImage from "src/components/HeroImage";
+import { contacts } from "src/data/contancts";
 
 export default function iletisim() {
   return (
@@ -9,21 +10,42 @@ export default function iletisim() {
         src="https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1471&q=80"
         text="İletişim"
       />
-      <div className="container mt-10">
+      <div className="container my-10">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3198.2943133225517!2d37.12051010131836!3d36.715492248535156!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x152fce1b6092e35b%3A0x55f8a8ad272ab16a!2sKilis%20Kona%C4%9F%C4%B1%20Butik%20Otel!5e0!3m2!1sen!2str!4v1633854569872!5m2!1sen!2str"
           className="w-full h-[500px]"
           loading="lazy"
         ></iframe>
-        <div>
-          <div>
+        <div className="flex mt-8 gap-10">
+          <div className="w-1/2">
             <Form />
           </div>
-          <div>
-            <p>address</p>
+          <div className="w-1/2">
+            <IconWithInfo
+              text={contacts.address}
+              icon="fas fa-map-marker-alt text-mainYello "
+            />
+            <IconWithInfo
+              text={contacts.phone}
+              icon="fas fa-phone text-mainYello "
+            />
+
+            <IconWithInfo
+              text={contacts.email}
+              icon="fas fa-envelope-open-text text-mainYello "
+            />
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const IconWithInfo = ({ text, icon }: any) => {
+  return (
+    <div className="flex items-center gap-4 mb-8">
+      <i className={icon}></i>
+      <p>{text}</p>
+    </div>
+  );
+};
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+const formStyle = "block w-full border py-2 my-4 p-2 rounded-md   ";
 export default function Form() {
-    return (
-        <div>
-            Form
-        </div>
-    )
+  return (
+    <form action="https://formspree.io/f/xknkgebk" method="POST">
+      <Input name="Adınız" placeholder="adınız" />
+      <Input name="TelefonNumaranız" placeholder="Telefon Numaranız" />
+      <Input name="E-postaAdresiniz" placeholder="E-posta Adresiniz" />
+      <textarea
+        name="Mesajınız"
+        placeholder="Mesajınız"
+        className={formStyle}
+      />
+      <button className={`${formStyle} bg-mainYello text-white`}>Gönder</button>
+    </form>
+  );
 }
+
+const Input = ({ name, placeholder }: any) => {
+  return <input className={formStyle} name={name} placeholder={placeholder} />;
+};
